e2aed70c578eba7777044e15083c320aeae598f7
DrPrunes <ric.carden@gmail.com>
2021-12-13 11:09:39 +0000

    added exit on reboot check True

--- all_checks.py
+++ all_checks.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import os
-
+import sys
 
 def check_reboot():
 	''' Returns True if theres na pending reboot '''
 	return os.path.exist("/run/reboot-required")
 def main():
-	pass
+	if check_reboot():
+		print("Pending Reboot")
+		sys.exit(1)
 
 main()
