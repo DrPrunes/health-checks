cb37e18893e162234875c405c2116b4a770c3542
DrPrunes <ric.carden@gmail.com>
2021-12-13 13:24:22 +0000

    now working script

--- all_checks.py
+++ all_checks.py
@@ -4,12 +4,12 @@ import sys
 
 def check_reboot():
 	''' Returns True if theres na pending reboot '''
-	return os.path.exist("/run/reboot-required")
+	return os.path.exists("/run/reboot-required")
 def main():
 	if check_reboot():
 		print("Pending Reboot")
 		sys.exit(1)
-		print("Evderything OK."
-		sys.exit(0)
+	print("Evderything OK.")
+	sys.exit(0)
 
 main()
