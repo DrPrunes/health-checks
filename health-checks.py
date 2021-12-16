0ddda31ab337dbbace43ec9672c6c921faf7cfa1
DrPrunes <ric.carden@gmail.com>
2021-12-16 11:53:38 +0000

    ren health-checks

---
+++ health-checks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import os
+import sys
+import csv
+
+def check_reboot():
+	''' Returns True if theres na pending reboot '''
+	return os.path.exists("/run/reboot-required")
+
+def dummy_function():
+	pass
+
+def dummy_function3():
+	pass
+
+
+def dummy_function2():
+	pass
+# addded this after merging  and deleting branch refactor
+
+def main():
+	if check_reboot():
+		print("Pending Reboot")
+		sys.exit(1)
+
+	print("Everything OK. Continue")
+	sys.exit(0)
+
+main()
+''' added this comment in refactor '''
